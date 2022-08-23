170915af69daaf24079143df7e4cfc32c69257b6
Antoine <titoinelemagnifique@gmail.com>
2022-08-23 13:11:49 -0400

    asd

---
+++ src/yeah.java
@@ -0,0 +1,4 @@
+
+public class yeah {
+    public static short testParameter(short parameter)
+    { System.out.println("Je suis une méthode qui travaille sur les short"); return (short) (parameter / 2); } public static int testParameter(int parameter) { System.out.println("Je suis une méthode qui travaille sur les int"); return parameter / 2; } public static float testParameter(float parameter) { System.out.println("Je suis une méthode qui travaille sur les float"); return parameter / 2f; } public static double testParameter(double parameter) { System.out.println("Je suis une méthode qui travaille sur les double"); return parameter / 2.0; } }
